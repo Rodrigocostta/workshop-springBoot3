d11c99ebb67e3401c553376e08d4fc21f81e1e4f
Rodrigo Costa <rhodrigo_costanet@hotmail.com>
2023-03-23 21:28:51 -0300

    creation entities(User) and resource(UserResource)

---
+++ src/main/java/com/rcosta/workShop/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.rcosta.workShop.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rcosta.workShop.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	ResponseEntity<User> findall() {
+		User u = new User(1L, "ze", "ze@Gmail", "4578965", "123");
+		return ResponseEntity.ok().body(u);
+
+	}
+
+}
